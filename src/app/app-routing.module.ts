fdf0df711de6ac5a38e59d75a2800db2c191dcb9
João Vitor <jv17fc@gmail.com>
2020-05-07 18:02:40 -0300

    Criando estrutura e organizando rotas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -11,6 +11,10 @@ const routes: Routes = [
     redirectTo: 'home',
     pathMatch: 'full'
   },
+  {
+    path: 'listas',
+    loadChildren: () => import('./listas/listas.module').then( m => m.ListasPageModule)
+  },
 ];
 
 @NgModule({
